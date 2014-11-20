a77fdf5899d345457a60ae82191eb257d7d4d6c9
limanyi <545387015@qq.com>
2014-11-20 20:53:19 +0800

    add main

---
+++ main.c
@@ -0,0 +1,79 @@
+﻿1.# include<stdio.h>
+void  main()
+{
+char a, b,c;
+a=’B’;b=’O’;c=’Y’
+putchar(a);putchar(b);putchar(c);putchar(‘\n’);
+}
+2. # include<stdio.h>
+void  main()
+{
+char a, b,c;
+a=’B’;b=’O’;c=’Y’
+putchar(a);putchar(‘\n’;)putchar(b);putchar(‘\n’);putchar(c);putchar(‘\n’);
+}
+3.# include<stdio.h>
+void  main()
+{
+char c=’a’;
+int i=97;
+printf(“%c,%d\n”,c,c);
+printf(“%c,%d\n”,i,i);
+}
+4. # include<stdio.h>
+void  main()
+{
+Float x,y;
+X=111111.111;222222.222;
+printf(“%f\n”,x+y);
+}
+5. # include<stdio.h>
+void  main()
+{
+double x,y;
+x=1111111111111.111111111;
+y=2222222222222.222222222;
+printf(“%f\n”,x+y);
+}
+6. # include<stdio.h>
+void  main()
+{
+float a,b,t;
+scanf (“%f,%f”,&a,&b);
+if(a>b)
+{
+t=a;
+a=b;
+b=t;
+}
+printf(“%5.2f,%5.2f\n”,a,b);
+}
+7. # include<stdio.h>
+void  main()
+{
+int x,y;
+scanf(“%d”,&x);
+if(x<0)
+ y=-1;
+else
+if(x==0) y=0;
+else y=1;
+printf(“x=%d,y=%d\n”,x,y);
+}
+8. # include<stdio.h>
+void  main()
+{
+int x,y;
+scanf(“%d”,&x);
+if(x>=0)
+if(x>0)  y=1;
+else y=0;
+else y=-1;
+printf(“x=%d,y=%d\n”,x,y);
+}
+
+
+
+
+
+
